9ff00c2cd066fcadf860ef61960dcb322a93c096
Justin Joyce <justinwjoyce@gmail.com>
2018-10-30 08:22:58 -0400

    rps

---
+++ app/controllers/rps_controller.rb
@@ -0,0 +1,25 @@
+class RpsController < ApplicationController
+  def throw
+    @user_move = params[:sign]
+    @computer_move = "Need to generate random throw"
+    @result = "You win or you lose"
+
+    if @user_move == @computer_move
+    @result = "You Tied"
+    elsif @user_move == "paper" && @computer_move == "rock"
+    @result = "You Win"
+    elsif @user_move == "paper" && @computer_move == "scissors"
+    @result = "You Lose"
+    elsif @user_move == "scissors" && @computer_move == "rock"
+    @result = "You Lose"
+    elsif @user_move == "scissors" && @computer_move == "paper"
+    @result = "You Win"
+    elsif @user_move == "rock" && @computer_move == "paper"
+    @result = "You Lose"
+    elsif @user_move == "rock" && @computer_move == "scissors"
+    @result = "You Win"
+    end
+
+    render("rps.html.erb")
+  end
+end
