99b70681b3aec7c4c9599a1c52b824e533ea22c0
Justin Joyce <justinwjoyce@gmail.com>
2018-11-05 16:12:06 -0500

    working version

--- app/controllers/rps_controller.rb
+++ app/controllers/rps_controller.rb
@@ -1,24 +1,24 @@
 class RpsController < ApplicationController
   def throw
     @user_move = params[:sign]
-    @computer_move = "Need to generate random throw"
+    @computer_move = ["rock","paper","scissors"].sample
     @result = "You win or you lose"
 
-    if @user_move == @computer_move
-    @result = "You Tied"
-    elsif @user_move == "paper" && @computer_move == "rock"
-    @result = "You Win"
-    elsif @user_move == "paper" && @computer_move == "scissors"
-    @result = "You Lose"
-    elsif @user_move == "scissors" && @computer_move == "rock"
-    @result = "You Lose"
-    elsif @user_move == "scissors" && @computer_move == "paper"
-    @result = "You Win"
-    elsif @user_move == "rock" && @computer_move == "paper"
-    @result = "You Lose"
-    elsif @user_move == "rock" && @computer_move == "scissors"
-    @result = "You Win"
-    end
+        if @user_move == @computer_move
+            @result = "You Tied"
+        elsif @user_move == "paper" && @computer_move == "rock"
+            @result = "You Win"
+        elsif @user_move == "paper" && @computer_move == "scissors"
+            @result = "You Lose"
+        elsif @user_move == "scissors" && @computer_move == "rock"
+            @result = "You Lose"
+        elsif @user_move == "scissors" && @computer_move == "paper"
+            @result = "You Win"
+        elsif @user_move == "rock" && @computer_move == "paper"
+            @result = "You Lose"
+        elsif @user_move == "rock" && @computer_move == "scissors"
+            @result = "You Win"
+        end
 
     render("rps.html.erb")
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'rps#throw'
+  get("/throw/:sign", { :controller => "rps", :action => "throw" })
   post("/throw/:sign", { :controller => "rps", :action => "throw" })
 end
